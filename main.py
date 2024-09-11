d552cf46f39ec5f03a39d28e9d9b2c44edc927aa
Dimanerter <erterujamp@gmail.com>
2024-09-11 18:53:23 +0200

    for

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-pets = ["dog", "fish", "cat"]
-
-match pets:
-    case ["dog", "cat", _]:
-        # Випадок, коли є і собака, і кіт
-        print("There's a dog and a cat.")
-    case ["dog", _, _]:
-        # Випадок, коли є тільки собака
-        print("There's a dog.")
-    case _:
-        # Випадок для інших комбінацій
-        print("No dogs.")
+fruit = 'apple'
+for char in fruit:
+    print(char)
